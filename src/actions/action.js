4efeaa7fcf6587cdd21038ae4fa2c92fecbfb044
Robel Tekle <robel.tekle@gmail.com>
2018-02-26 22:13:13 +0100

    The array data from Wikipedia Api are been converted to an Object, tha is a collection of objects, with several keys

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,9 +1,21 @@
 import axios from "axios";
 // import ReduxThunk from "redux-thunk";
 
-
-
 export const SEARCH_TERM = "search_term";
+
+
+
+function fromArrayToObj(array) {
+    let obj = {};
+    for(var i = 0; i < array[1].length; i++){
+        obj = {...obj, [array[1][i]] : {
+            term : array[1][i], 
+            expl : array[2][i],
+            link : array[3][i]    
+        } } 
+    };
+    return obj;
+}
 
 
 export function Search(lang, term){
@@ -11,19 +23,11 @@ export function Search(lang, term){
     const responce = axios.get(url);
     return (dispatch)=>{
         responce.then(({data})=>{
-            console.log(data);
-            dispatch({type: "search_term", payload: data})
+            const dataObj = fromArrayToObj(data);
+            console.log(dataObj);
+            dispatch({type: "search_term", payload: dataObj})
 
         })
     }
 
 }
-
-// export function Search(lang, term){
-//         const url = `https://${lang}.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${term}&limit=6&format=json`;
-//         axios.get(url)
-//             .then((resp)=>{
-//                 console.log(resp.data);
-//                 return{type: "search_term", payload: resp.data }
-//             })
-// }
