1b50307046c6d93e9f0e32347a943a80ad1ebfc1
Robel Tekle <robel.tekle@gmail.com>
2018-02-27 14:05:02 +0100

    Search Action has 3 options: no terme (at begining or if user cancel terme) return {}; no match (if the api doesn't find any match) return error; search terme (if the api find a match) return the data

--- src/actions/action.js
+++ src/actions/action.js
@@ -2,12 +2,14 @@ import axios from "axios";
 // import ReduxThunk from "redux-thunk";
 
 export const SEARCH_TERM = "search_term";
+export const NO_MATCH = "no_match";
+export const NO_TERME = "no_terme";
 
 
 
 function fromArrayToObj(array) {
     let obj = {};
-    for(var i = 0; i < array[1].length; i++){
+    for(var i = 0; i < 6; i++){
         obj = {...obj, [array[1][i]] : {
             term : array[1][i], 
             expl : array[2][i],
@@ -19,13 +21,23 @@ function fromArrayToObj(array) {
 
 
 export function Search(lang, term){
+    //When the term === "" return an empty object 
+    if(term===""){
+        return {type: NO_TERME, payload: {}}
+    };
+    //When the term !== ""
     const url = `https://${lang}.wikipedia.org/w/api.php?origin=*&action=opensearch&search=${term}&limit=6&format=json`;
     const responce = axios.get(url);
     return (dispatch)=>{
         responce.then(({data})=>{
             const dataObj = fromArrayToObj(data);
+            //If there is no match return an error 
             console.log(dataObj);
-            dispatch({type: "search_term", payload: dataObj})
+            if(dataObj.undefined){
+                dispatch({type: NO_MATCH, payload: {error : "no match"}})
+            } 
+            //Otherwise dipatch dataObj
+            else { dispatch({type: SEARCH_TERM, payload: dataObj})};
 
         })
     }
--- src/reducers/reducer_responce.js
+++ src/reducers/reducer_responce.js
@@ -1,11 +1,17 @@
 import {SEARCH_TERM} from "../actions/action";
+import {NO_MATCH} from "../actions/action";
+import {NO_TERME} from "../actions/action";
 
 
 export default function(state = {}, action){
     switch (action.type) {
         case SEARCH_TERM:
         return action.payload;
+        case NO_TERME:
+        return action.payload;
+        case NO_MATCH:
+        return action.payload;
         default:
-        return "ERRORE";
+        return state;
     }
 }
